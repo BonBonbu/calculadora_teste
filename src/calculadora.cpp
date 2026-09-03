7f2e35a5141b917a69d6b576bc53c417dc61748d
katsuyaki <tsu.haru.by@gmail.com>
2026-09-02 23:16:35 -0300

    Feature: busca de parenteses, inciando metodo de prioridades

--- src/calculadora.cpp
+++ src/calculadora.cpp
@@ -1,67 +1,105 @@
 #include <stdio.h>
 #include <locale.h>
+#include <math.h>
+#include <stdlib.h>
 
-float Num1, Num2;
-char operador;
+int verifica_parenteses(char conta[], int inicio);
+double multiplicacao(double numero1, double numero2);
+double divisao(double numero, double divisor);
+double adicao(double numero1, double numero2);
 
-float Soma(float Num1, float Num2){
-    return Num1 + Num2;
-}
+int main()
+{
+    int i = 0, j = 0, parenteses_inicial = 0, parenteses_final = 0, operador;
+    char conta[101] = {0};
+    double numero1[20], numero2[20];
 
-float Subitracao(float Num1, float Num2){
-    return Num1 - Num2;
-}
+    printf("Conta a ser efetuada: ");
+    //sem espacos pois nao tem em calculadoras
+    //limite de 100 caracteres
+    fgets(conta, 101, stdin);
+    //armazena a conta e termina em /0
 
-float divisao(float Num1, float Num2){
-   return Num1 / Num2;
-}
+    while(conta[i] != '\0' && conta[i] != '\n')
+    {
 
-float multiplicacao(float Num1, float Num2){
-    return Num1 * Num2;
-}
+        //prioridade (raiz e potencia)
 
-int main(){
+        if(conta[i] == '^')
+        {
 
-    printf("Digite primeiro numero: \n");
-    scanf("%f", &Num1);
-
-    while(1){ /*o while tem que ser 1 para ser verdadeira*/
-        printf("Digite operador: \n");
-        scanf(" %c", &operador);
-
-        if(operador == '='){
-            break;
         }
 
-        printf("Digite segundo numero: \n");
-        scanf("%f", &Num2);
-        switch (operador)
+        //10+2*(4+10)
+        //prioridade dos parenteses
+
+        if(conta[i] == '(')
         {
-        case '+':
-            Num1 = Soma(Num1, Num2);
-            break;
-        case '-':
-            Num1 = Subitracao(Num1, Num2);
-            break;
-        case '/':
-            if(Num2 != 0){
-                Num1 = divisao(Num1, Num2);
+            parenteses_inicial = i;
+            parenteses_final = verifica_parenteses(conta, i);
+            //vai ler desde o inicial ate fechar em 0 pois achou o final
+
+            if(parenteses_final == NAN)
+            {
+                printf("Parenteses nao fechado\n");
+                return printf("Erro\n");
             }
-            else{
-                printf("ERRO! Nao e possivel dividir por zero.\n");
-                continue;
-            }
-            break;
-        case '*':
-            Num1 = multiplicacao(Num1, Num2);
-            break;
-        default: 
-            printf("ERRO! Operador invalido.\n");
-            continue;
+
         }
     }
 
-    printf("Resultado: %.2f\n", Num1);
-
     return 0;
+}
+
+int verifica_parenteses(char conta[], int inicio)
+{
+    int parenteses_contagem = 1, i = inicio + 1; 
+    //i = inicio para menor confusao
+
+    while(conta[i] != '\0')
+    {
+        if(conta[i] == '(')
+        {
+            parenteses_contagem++;
+        }
+
+        if(conta[i] == ')')
+        {
+            parenteses_contagem--;
+
+            if(parenteses_contagem == 0)
+            {
+                return i;
+            }
+        }
+
+        i++;
+    }
+
+    return -1; //erro
+}
+
+double multiplicacao(double numero1, double numero2)
+{
+    return numero1 * numero2;
+}
+
+double divisao(double numero, double divisor)
+{
+    if(divisor != 0)
+    {
+        return numero / divisor;
+    }
+    
+    return NAN; //erro
+}
+
+double adicao(double numero1, double numero2)
+{
+    return numero1 + numero2;
+}
+
+double subtracao(double numero1, double numero2)
+{
+    return numero1 - numero2;
 }
